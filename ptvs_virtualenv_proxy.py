de1dcca0277cf2e1ccf6064f3f97eab79c7f128a
Annie Fu <anniefu@umich.edu>
2017-03-25 21:39:28 -0400

    Update file structures to match Flask template example.

--- ptvs_virtualenv_proxy.py
+++ ptvs_virtualenv_proxy.py
@@ -1,44 +1,122 @@
-<?xml version="1.0"?>
-<configuration>
-  <appSettings>
-    <add key="WSGI_ALT_VIRTUALENV_HANDLER" value="app.wsgi_app" />
-    <add key="WSGI_ALT_VIRTUALENV_ACTIVATE_THIS"
-         value="D:\home\site\wwwroot\env\Scripts\python.exe" />
-    <add key="WSGI_HANDLER"
-         value="ptvs_virtualenv_proxy.get_venv_handler()" />
-    <add key="PYTHONPATH" value="D:\home\site\wwwroot" />
-  </appSettings>
-  <system.web>
-    <compilation debug="true" targetFramework="4.0" />
-  </system.web>
-  <system.webServer>
-    <modules runAllManagedModulesForAllRequests="true" />
-    <handlers>
-      <remove name="Python27_via_FastCGI" />
-      <remove name="Python34_via_FastCGI" />
-      <add name="Python FastCGI"
-           path="handler.fcgi"
-           verb="*"
-           modules="FastCgiModule"
-           scriptProcessor="D:\Python34\python.exe|D:\Python34\Scripts\wfastcgi.py"
-           resourceType="Unspecified"
-           requireAccess="Script" />
-    </handlers>
-    <rewrite>
-      <rules>
-        <rule name="Static Files" stopProcessing="true">
-          <conditions>
-            <add input="true" pattern="false" />
-          </conditions>
-        </rule>
-        <rule name="Configure Python" stopProcessing="true">
-          <match url="(.*)" ignoreCase="false" />
-          <conditions>
-            <add input="{REQUEST_URI}" pattern="^/static/.*" ignoreCase="true" negate="true" />
-          </conditions>
-          <action type="Rewrite" url="handler.fcgi/{R:1}" appendQueryString="true" />
-        </rule>
-      </rules>
-    </rewrite>
-  </system.webServer>
-</configuration>
+﻿ # ############################################################################
+ #
+ # Copyright (c) Microsoft Corporation. 
+ #
+ # This source code is subject to terms and conditions of the Apache License, Version 2.0. A 
+ # copy of the license can be found in the License.html file at the root of this distribution. If 
+ # you cannot locate the Apache License, Version 2.0, please send an email to 
+ # vspython@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+ # by the terms of the Apache License, Version 2.0.
+ #
+ # You must not remove this notice, or any other, from this software.
+ #
+ # ###########################################################################
+
+import datetime
+import os
+import sys
+import traceback
+
+if sys.version_info[0] == 3:
+    def to_str(value):
+        return value.decode(sys.getfilesystemencoding())
+
+    def execfile(path, global_dict):
+        """Execute a file"""
+        with open(path, 'r') as f:
+            code = f.read()
+        code = code.replace('\r\n', '\n') + '\n'
+        exec(code, global_dict)
+else:
+    def to_str(value):
+        return value.encode(sys.getfilesystemencoding())
+
+def log(txt):
+    """Logs fatal errors to a log file if WSGI_LOG env var is defined"""
+    log_file = os.environ.get('WSGI_LOG')
+    if log_file:
+        f = open(log_file, 'a+')
+        try:
+            f.write('%s: %s' % (datetime.datetime.now(), txt))
+        finally:
+            f.close()
+
+ptvsd_secret = os.getenv('WSGI_PTVSD_SECRET')
+if ptvsd_secret:
+    log('Enabling ptvsd ...\n')
+    try:
+        import ptvsd
+        try:
+            ptvsd.enable_attach(ptvsd_secret)
+            log('ptvsd enabled.\n')
+        except: 
+            log('ptvsd.enable_attach failed\n')
+    except ImportError:
+        log('error importing ptvsd.\n')
+
+def get_wsgi_handler(handler_name):
+    if not handler_name:
+        raise Exception('WSGI_ALT_VIRTUALENV_HANDLER env var must be set')
+    
+    if not isinstance(handler_name, str):
+        handler_name = to_str(handler_name)
+    
+    module_name, _, callable_name = handler_name.rpartition('.')
+    should_call = callable_name.endswith('()')
+    callable_name = callable_name[:-2] if should_call else callable_name
+    name_list = [(callable_name, should_call)]
+    handler = None
+    last_tb = ''
+
+    while module_name:
+        try:
+            handler = __import__(module_name, fromlist=[name_list[0][0]])
+            last_tb = ''
+            for name, should_call in name_list:
+                handler = getattr(handler, name)
+                if should_call:
+                    handler = handler()
+            break
+        except ImportError:
+            module_name, _, callable_name = module_name.rpartition('.')
+            should_call = callable_name.endswith('()')
+            callable_name = callable_name[:-2] if should_call else callable_name
+            name_list.insert(0, (callable_name, should_call))
+            handler = None
+            last_tb = ': ' + traceback.format_exc()
+    
+    if handler is None:
+        raise ValueError('"%s" could not be imported%s' % (handler_name, last_tb))
+    
+    return handler
+
+activate_this = os.getenv('WSGI_ALT_VIRTUALENV_ACTIVATE_THIS')
+if not activate_this:
+    raise Exception('WSGI_ALT_VIRTUALENV_ACTIVATE_THIS is not set')
+
+def get_virtualenv_handler():
+    log('Activating virtualenv with %s\n' % activate_this)
+    execfile(activate_this, dict(__file__=activate_this))
+
+    log('Getting handler %s\n' % os.getenv('WSGI_ALT_VIRTUALENV_HANDLER'))
+    handler = get_wsgi_handler(os.getenv('WSGI_ALT_VIRTUALENV_HANDLER'))
+    log('Got handler: %r\n' % handler)
+    return handler
+
+def get_venv_handler():
+    log('Activating venv with executable at %s\n' % activate_this)
+    import site
+    sys.executable = activate_this
+    old_sys_path, sys.path = sys.path, []
+    
+    site.main()
+    
+    sys.path.insert(0, '')
+    for item in old_sys_path:
+        if item not in sys.path:
+            sys.path.append(item)
+
+    log('Getting handler %s\n' % os.getenv('WSGI_ALT_VIRTUALENV_HANDLER'))
+    handler = get_wsgi_handler(os.getenv('WSGI_ALT_VIRTUALENV_HANDLER'))
+    log('Got handler: %r\n' % handler)
+    return handler
--- runserver.py
+++ runserver.py
@@ -1,20 +1,14 @@
-from flask import Flask, render_template
-import habla.controllers as controllers
-import habla.api as api
-import habla.config as config
+"""
+This script runs the FlaskWebProject1 application using a development server.
+"""
 
-# Initialize Flask app with the template folder address
-app = Flask(__name__, template_folder='templates')
+from os import environ
+from habla import app
 
-# Register the controllers
-
-app.register_blueprint(api.comments)
-app.register_blueprint(api.groups)
-
-app.secret_key = 'C\xafh\xf8\xfd0m\xa8\xf3\xe3\x14,\xda\xa6\xb8\xffq\x8bm\xb9\xc2\x02\xfc\x19'
-
-# Listen on external IPs
-# For us, listen to port 3000 so you can just run 'python app.py' to start the server
 if __name__ == '__main__':
-    # listen on external IPs
-    app.run(host=config.env['host'], port=config.env['port'], debug=True)
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
